edf57ead511e984b41691b9b2f5624a32cc5681e
alejandroJL93 <alejandrojimenezlopez93@gmail.com>
2024-11-06 13:04:14 +0100

    Creacion home page y debuggers

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route]
-class MainController
+
+class MainController extends AbstractController
 {
-    public function homepage()
+    #[Route('/')]
+    public function homepage() : Response
     {
+        return $this->render(view: ('main/homepage.html.twig'));
     }
 }
